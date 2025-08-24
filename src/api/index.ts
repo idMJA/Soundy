5d370c52fe1923086e007970b33f9d27b0801edd
iaMJ <kiyomi@mjba.my>
2025-08-24 22:48:37 +0700

    feat: temp WebSocket server for player state management and event handling

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,5 +2,7 @@ export * from "./server";
 export * from "./music";
 export * from "./top";
 export * from "./playlist";
-export * from "./ws/";
+export * from "./ws";
 export * from "./types";
+
+// ws dir will fixed soon if im not bored, still have the issue atm, will fixed i have a mood. use ws.ts for now
