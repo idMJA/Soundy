5cffc0c0dac022b9b3f8c47b39507be76ff3a4e6
iaMJ <kiyomi@mjba.my>
2025-09-05 23:42:04 +0700

    feat: add config validation to initialization process

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import "@dotenvx/dotenvx/config";
 import Soundy from "#soundy/client";
 import { Logger } from "seyfert";
-import { SoundyLogger, validateEnv } from "#soundy/utils";
+import { SoundyLogger, validateEnv, validateConfig } from "#soundy/utils";
 import { APIServer } from "#soundy/api";
 
 Logger.customize(SoundyLogger);
 
 validateEnv();
+validateConfig();
 
 const client = new Soundy();
 APIServer(client);
