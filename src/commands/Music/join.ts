65922ff0347cdf8b6c2104f2c23576fafd689f6c
iaMJ <kiyomi@mjba.my>
2026-01-24 17:16:44 +0700

    chore: update version and remove unaccesary await

--- src/commands/Music/join.ts
+++ src/commands/Music/join.ts
@@ -27,18 +27,16 @@ export default class JoinCommand extends Command {
 		const guild = await ctx.guild();
 		if (!guild) return;
 
-		const member = await ctx.member;
+		const member = ctx.member;
 		if (!member) return;
 
 		const voiceChannel = (await member.voice()).channelId;
 		if (!voiceChannel) return;
 
-		// Check if a player already exists
 		let player = client.manager.getPlayer(guild.id);
 
-		// If no player exists, create one and connect to the voice channel
 		if (!player) {
-			player = await client.manager.createPlayer({
+			player = client.manager.createPlayer({
 				guildId: guild.id,
 				voiceChannelId: voiceChannel,
 				textChannelId: ctx.channelId,
